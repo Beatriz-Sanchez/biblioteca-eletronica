323bfacc0243b2df1991bcaa6e862ef23f1e271b
Beatriz Lisboa Sanchez <bia.lsanchez@gmail.com>
2022-05-26 13:19:20 -0300

    barra de navegação adicionada

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
 import React, {Component} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import BottomTabNavigator from './components/BottomTabNavigator';
 
 export default class App extends Component{
   render(){
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <BottomTabNavigator />
     );
   }
 }
