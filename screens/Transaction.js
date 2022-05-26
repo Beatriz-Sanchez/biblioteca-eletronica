67e2bcbca75e7ae6da739a1045624a26bd59dddb
Beatriz Lisboa Sanchez <bia.lsanchez@gmail.com>
2022-05-26 13:48:11 -0300

    adição do scanner de QR code

--- screens/Transaction.js
+++ screens/Transaction.js
@@ -1,11 +1,48 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
 export default class TransactionScreen extends Component {
+  constructor(){
+    super();
+    this.state = {
+      domState: "normal",
+      hasCameraPermissions: null,
+      scanned: false,
+      scannedData: "",
+    }
+  }
+  getCameraPermission = async (domState) => {
+    const { status } = await BarCodeScanner.requestPermissionsAsync()
+    this.setState({
+      domState: domState,
+      hasCameraPermissions: status === "granted",
+      scanned: false,
+      scannedData: "",
+    });
+  }
+  handleBarCodeScanned = async ({ type, data }) => {
+    this.setState({
+      scannedData: data,
+      domState: "normal",
+      scanned: true
+    })
+  }
   render() {
+    const { domState, hasCameraPermissions, scannedData, scanned } = this.state
+    if(domState === "scanner"){
+      return(
+            <BarCodeScanner onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned} style={StyleSheet.absoluteFillObject}/>
+        )
+    }
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>Tela de Transação</Text>
+        <Text style={styles.text}>
+        {hasCameraPermissions ? scannedData: "Conceda acesso à câmera"}
+        </Text>
+        <TouchableOpacity style={styles.button} onPress={()=>{this.getCameraPermission("scanner")}}>
+        <Text style={styles.buttonText}>Escanear{'\n'}QR code</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -21,5 +58,17 @@ const styles = StyleSheet.create({
   text: {
     color: "#ffff",
     fontSize: 30
+  },
+  buttonText: {
+    color: "#ffff",
+    fontSize: 24,
+    textAlign: "center"
+  },
+  button:{
+    width:"43%",
+    justifyContent:"center",
+    alignItems:"center",
+    backgroundColor: "#F48D20",
+    borderRadius:15
   }
 });
