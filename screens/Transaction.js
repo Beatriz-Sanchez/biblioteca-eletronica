68b7c25a36352f5aec290b3c2dd1e3e938203b53
Beatriz Lisboa Sanchez <bia.lsanchez@gmail.com>
2022-06-07 13:02:28 -0300

    logica de transaçoes completa

--- screens/Transaction.js
+++ screens/Transaction.js
@@ -20,15 +20,19 @@ import {
   collection,
   serverTimestamp,
   increment,
+  query,
+  where,
+  getDocs,
+  limit
 } from "firebase/firestore";
 //toast p/ iPhone
-import { RootSiblingParent } from 'react-native-root-siblings';
-import Toast from 'react-native-root-toast'
+import { RootSiblingParent } from "react-native-root-siblings";
+import Toast from "react-native-root-toast";
 
 import { db } from "../config";
 
 //para evitar avisos de timer
-LogBox.ignoreLogs(['Setting a timer']);
+LogBox.ignoreLogs(["Setting a timer"]);
 
 const bgImage = require("../assets/background2.png");
 const appIcon = require("../assets/appIcon.png");
@@ -86,19 +90,28 @@ export default class TransactionScreen extends Component {
     var transactionType = await this.checkBookAvailability(bookId);
 
     if (transactionType == "issue") {
-      this.initiateBookIssue(bookId, studentId, bookName, studentName);
+      var isEligible = await this.checkStudentEligibilityForBookIssue(
+        studentId
+      );
+      if (isEligible) {
+        this.initiateBookIssue(bookId, studentId, bookName, studentName);
+      }
     } else if (transactionType == "return") {
-      this.InitiateBookReturn(bookId, studentId, bookName, studentName);
+      var isEligible = await this.checkStudentEligibilityForBookReturn(
+        bookId,
+        studentId
+      );
+      if (isEligible) {
+        this.initiateBookReturn(bookId, studentId, bookName, studentName);
+      }
     } else if (!transactionType) {
-
       //toast para Android
-      //ToastAndroid.show("Livro não encontrado, cheque o ID e tente novamente", ToastAndroid.SHORT);
+      //ToastAndroid.show("Livro não encontrado, cheque o ID e tente novamente", ToastAndroid.LONG);
 
       //toast para iPhone
-      Toast.show('Livro não encontrado, cheque o ID e tente novamente', {
-        duration: Toast.durations.SHORT,
+      Toast.show("Livro não encontrado, cheque o ID e tente novamente", {
+        duration: Toast.durations.LONG,
       });
-
 
       this.setState({
         bookId: "",
@@ -133,17 +146,17 @@ export default class TransactionScreen extends Component {
       bookId: "",
       studentId: "",
     });
-    
+
     //toast para android
     //ToastAndroid.show("livro retirado", ToastAndroid.SHORT);
 
     //toast para iPhone
-    Toast.show('Livro retirado', {
+    Toast.show("Livro retirado", {
       duration: Toast.durations.SHORT,
     });
   };
 
-  InitiateBookReturn = (bookId, studentId, bookName, studentName) => {
+  initiateBookReturn = (bookId, studentId, bookName, studentName) => {
     var transactionsRef = collection(db, "transactions");
     var bookRef = doc(db, "books", bookId);
     var studentRef = doc(db, "students", studentId);
@@ -174,8 +187,7 @@ export default class TransactionScreen extends Component {
     //ToastAndroid.show("livro devolvido", ToastAndroid.SHORT);
 
     //toast para iPhone
-    //toast para iPhone
-    Toast.show('Livro devolvido', {
+    Toast.show("Livro devolvido", {
       duration: Toast.durations.SHORT,
     });
   };
@@ -220,6 +232,65 @@ export default class TransactionScreen extends Component {
 
     return transactionType;
   };
+
+  checkStudentEligibilityForBookIssue = async (studentId) => {
+    var studentRef = doc(db, "students", studentId);
+    var studentDoc = await getDoc(studentRef);
+
+    var isStudentEligible = "";
+
+    if (studentDoc.exists) {
+      if (studentDoc.data().number_of_books_issued < 2) {
+        isStudentEligible = true;
+      } else {
+        isStudentEligible = false;
+        //ToastAndroid.show("O aluno já atingiu o limite de 2 livros", ToastAndroid.LONG);
+        Toast.show("O aluno já atingiu o limite de 2 livros", {
+          duration: Toast.durations.LONG,
+        });
+        this.setState({
+          bookId: "",
+          studentId: "",
+        });
+      }
+    } else {
+      this.setState({ bookId: "", student: "" });
+      isStudentEligible = false;
+      //ToastAndroid.show("O ID do aluno não existe no banco de dados", ToastAndroid.LONG);
+      Toast.show("O ID do aluno não existe no banco de dados", {
+        duration: Toast.durations.LONG,
+      });
+    }
+
+    return isStudentEligible;
+  };
+
+  checkStudentEligibilityForBookReturn = async (bookId, studentId) => {
+    const transactionRef = query(
+      collection(db, "transactions"),
+      where("book_id", "==", bookId),
+      limit(1)
+    );
+    const docs = await getDocs(transactionRef);
+
+    var isStudentEligible;
+    docs.forEach((doc) => {
+      var lastBookTransaction = doc.data();
+
+      if (lastBookTransaction.student_id === studentId) {
+        isStudentEligible = true;
+      } else {
+        isStudentEligible = false;
+        //ToastAndroid.show("O livro não foi retirado por este aluno", ToastAndroid.SHORT);
+        Toast.show("O livro não foi retirado por este aluno", {
+          duration: Toast.durations.SHORT,
+        });
+      }
+    });
+
+    return isStudentEligible;
+  };
+
   render() {
     const { domState, hasCameraPermissions, bookId, studentId, scanned } =
       this.state;
@@ -233,55 +304,55 @@ export default class TransactionScreen extends Component {
     }
     return (
       <KeyboardAvoidingView behavior="height" style={styles.container}>
-      <RootSiblingParent>
-        <ImageBackground source={bgImage} style={styles.bgImage}>
-          <View style={styles.upperContainer}>
-            <Image source={appIcon} style={styles.appIcon} />
-            <Image source={appName} style={styles.appName} />
-          </View>
-          <View style={styles.lowerContainer}>
-            <View style={styles.textInputContainer}>
-              <TextInput
-                style={styles.textInput}
-                placeholder={"ID Livro"}
-                placeholderTextColor={"#ffffff"}
-                value={bookId}
-                onChangeText={text => this.setState({bookId: text})}
-              />
+        <RootSiblingParent>
+          <ImageBackground source={bgImage} style={styles.bgImage}>
+            <View style={styles.upperContainer}>
+              <Image source={appIcon} style={styles.appIcon} />
+              <Image source={appName} style={styles.appName} />
+            </View>
+            <View style={styles.lowerContainer}>
+              <View style={styles.textInputContainer}>
+                <TextInput
+                  style={styles.textInput}
+                  placeholder={"ID Livro"}
+                  placeholderTextColor={"#ffffff"}
+                  value={bookId}
+                  onChangeText={(text) => this.setState({ bookId: text })}
+                />
+                <TouchableOpacity
+                  style={styles.scanbutton}
+                  onPress={() => {
+                    this.getCameraPermission("bookId");
+                  }}
+                >
+                  <Text style={styles.scanbuttonText}>Digitalizar</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.textInputContainer}>
+                <TextInput
+                  style={styles.textInput}
+                  placeholder={"ID Aluno"}
+                  placeholderTextColor={"#ffffff"}
+                  value={studentId}
+                  onChangeText={(text) => this.setState({ studentId: text })}
+                />
+                <TouchableOpacity
+                  style={styles.scanbutton}
+                  onPress={() => {
+                    this.getCameraPermission("studentId");
+                  }}
+                >
+                  <Text style={styles.scanbuttonText}>Digitalizar</Text>
+                </TouchableOpacity>
+              </View>
               <TouchableOpacity
-                style={styles.scanbutton}
-                onPress={() => {
-                  this.getCameraPermission("bookId");
-                }}
+                style={styles.button}
+                onPress={this.handleTransaction}
               >
-                <Text style={styles.scanbuttonText}>Digitalizar</Text>
+                <Text style={styles.buttonText}>Enviar</Text>
               </TouchableOpacity>
             </View>
-            <View style={styles.textInputContainer}>
-              <TextInput
-                style={styles.textInput}
-                placeholder={"ID Aluno"}
-                placeholderTextColor={"#ffffff"}
-                value={studentId}
-                onChangeText={text => this.setState({studentId: text})}
-              />
-              <TouchableOpacity
-                style={styles.scanbutton}
-                onPress={() => {
-                  this.getCameraPermission("studentId");
-                }}
-              >
-                <Text style={styles.scanbuttonText}>Digitalizar</Text>
-              </TouchableOpacity>
-            </View>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={this.handleTransaction}
-            >
-              <Text style={styles.buttonText}>Enviar</Text>
-            </TouchableOpacity>
-          </View>
-        </ImageBackground>
+          </ImageBackground>
         </RootSiblingParent>
       </KeyboardAvoidingView>
     );
